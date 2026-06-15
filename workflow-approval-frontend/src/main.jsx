0d8d6bec7fdf166eb48d7d6788cdd6fc490922d3
Bogiri Venkatadhaneep <your-email@example.com>
2026-06-15 18:56:08 +0530

    GitHub Pages final fix

--- workflow-approval-frontend/src/main.jsx
+++ workflow-approval-frontend/src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(
-document.getElementById("root")
-).render(
-<React.StrictMode>
-<HashRouter>
-<App />
-</HashRouter>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
